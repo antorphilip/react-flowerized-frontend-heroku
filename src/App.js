341b73125dfa9f60b9e173e6e3467991624a8743
unknown <antor.philip@gmail.com>
2022-07-06 11:26:04 +0800

    change app js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Sana deployed na</h1>
+        <h1> HELLOO WORLD Sana deployed na</h1>
       </header>
     </div>
   );
