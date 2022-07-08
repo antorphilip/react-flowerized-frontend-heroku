992259d4b1d93ed867a255f1518ecb9f4c401eaf
unknown <antor.philip@gmail.com>
2022-07-08 21:53:03 +0800

    commiting REST

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import {useState,useEffect} from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/items", )
+      .then(response => response.json())
+      .then((data) => setItems(data));
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> HELLOO WORLD Sana deployed na</h1>
-      </header>
-    </div>
+    
+      <div>
+        <h1>Items</h1>
+        {items.map((item)=> (
+          <div key={item.id}>
+            <h1>id: {item.id}</h1>
+            <h1>name: {item.name}</h1>
+            <h1>created: {item.created}</h1>
+      </div>
+        ))}
+      </div>
   );
-}
+        }
 
 export default App;
