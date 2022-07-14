f1f7f790918996a44bff9b9f58bc05259673ec88
aeschyllus <lapadamarkanthony@gmail.com>
2022-07-14 12:32:28 +0800

    Fixed navbar responsiveness. Converted css to css module

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -22,94 +22,115 @@ function Navbar() {
     showButton();
   }, []);
 
-  window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
   return (
-    <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            FLOWERIZED
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                HOME
-              </Link>
-            </li>
-
-            <li className='nav-item'>
-              <Link
-                to='/events'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                EVENTS
-              </Link>
-            </li>
-
-            <li className='nav-item'>
-              <Link
-                to='/customize'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                CUSTOMIZE
-              </Link>
-            </li>
-
-            <li className='nav-item'>
-              <Link
-                to='/reviews'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                REVIEWS
-              </Link>
-            </li>
-
-            <li className='nav-item'>
-              <Link
-                to='/aboutUs'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                ABOUT US
-              </Link>
-            </li>
-
-            <li className='nav-item'>
-              <Link
-                to='/contactUs'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                CONTACT US
-              </Link>
-            </li>
-           
-
-            
-            <li>
-              <Link
-                to='/sign-up'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            </li>
-            </ul>
-            
-          
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+    <nav className={styles.navbar}>
+      {/* Title */}
+      <div className={styles["nav-header"]}>
+        <Link
+          to="/"
+          className={styles["navbar-logo"]}
+          onClick={closeMobileMenu}
+        >
+          FLOWERIZED
+        </Link>
+        <div className={styles["menu-icon"]} onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
-      </nav>
-    </>
+      </div>
+
+      {/* Links */}
+      <ul
+        className={
+          click
+            ? `${styles["nav-menu"]} ${styles.active}`
+            : `${styles["nav-menu"]}`
+        }
+      >
+        <li className={styles["nav-item"]}>
+          <Link
+            to="/"
+            className={styles["nav-links"]}
+            onClick={closeMobileMenu}
+          >
+            HOME
+          </Link>
+        </li>
+
+        <li className={styles["nav-item"]}>
+          <Link
+            to="/events"
+            className={styles["nav-links"]}
+            onClick={closeMobileMenu}
+          >
+            EVENTS
+          </Link>
+        </li>
+
+        <li className={styles["nav-item"]}>
+          <Link
+            to="/customize"
+            className={styles["nav-links"]}
+            onClick={closeMobileMenu}
+          >
+            CUSTOMIZE
+          </Link>
+        </li>
+
+        <li className={styles["nav-item"]}>
+          <Link
+            to="/customize"
+            className={styles["nav-links"]}
+            onClick={closeMobileMenu}
+          >
+            CUSTOMIZE
+          </Link>
+        </li>
+
+        <li className={styles["nav-item"]}>
+          <Link
+            to="/reviews"
+            className={styles["nav-links"]}
+            onClick={closeMobileMenu}
+          >
+            REVIEWS
+          </Link>
+        </li>
+
+        <li className={styles["nav-item"]}>
+          <Link
+            to="/aboutUs"
+            className={styles["nav-links"]}
+            onClick={closeMobileMenu}
+          >
+            ABOUT US
+          </Link>
+        </li>
+
+        <li className={styles["nav-item"]}>
+          <Link
+            to="/contactUs"
+            className={styles["nav-links"]}
+            onClick={closeMobileMenu}
+          >
+            CONTACT US
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="/sign-up"
+            className={styles["nav-links-mobile"]}
+            onClick={closeMobileMenu}
+          >
+            Sign Up
+          </Link>
+        </li>
+      </ul>
+
+      {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+    </nav>
   );
 }
 
