c2ea953e8830b2005b8310bef31bea76ad3eb710
unknown <kheyhan31@gmail.com>
2022-08-03 21:02:34 +0800

    nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -18,8 +18,8 @@ const NavBar = () => {
     <>
       <nav className="flex justify-between  items-center bg-gradient-to-r from-[#f5d3e4] to-[#ecdce4] h-20">
         <div className="flex items-center">
-          <img className="h-16 ml-4" src="./image/logo.png" alt="logo" />
-          <h1 className="font-light text-[#333f27] tracking-[10px] uppercase text-4xl font-Cinzel ml-4 mt-2">
+          <img className="h-16" src="./image/logo.png" alt="logo" />
+          <h1 className="font-light text-[#333f27] tracking-[10px] uppercase text-3xl xl:text-4xl font-Cinzel mt-2">
             Flowerized
           </h1>
         </div>
@@ -27,8 +27,8 @@ const NavBar = () => {
         <ul className="md:flex hidden">
           {navLinks.map(function (navLink) {
             return (
-              <li className="navLink">
-                <Link to={navLink.path} className="navLink">
+              <li className="navsLink">
+                <Link to={navLink.path} className="navsLink">
                   {navLink.title}
                 </Link>
               </li>
@@ -37,23 +37,27 @@ const NavBar = () => {
         </ul>
 
         <div>
-          <button className="md:hidden" onClick={() => setNav(!nav)}>
+          <button className="mr-[20px] md:hidden" onClick={() => setNav(!nav)}>
             {nav ? (
               <i class="fa-solid fa-x fa-2x"></i>
             ) : (
               <i className="fa-solid fa-bars fa-2x"></i>
             )}
           </button>
-          <a className="hidden md:block" href="#Login">
+          <a href="/login#Login" className="hidden md:block">
             <i className="fa-regular fa-circle-user fa-2x iconColor mr-[30px]"></i>
           </a>
         </div>
       </nav>
-      <ul className={`md:hidden bg-[#f5d3e4] ${nav ? "block" : "hidden"}`}>
+      <ul
+        className={`text-center pl-0 md:hidden bg-[#f5d3e4]/[0.6] ${
+          nav ? "fixed w-full" : "hidden"
+        }`}
+      >
         {navLinks.map(function (navLink) {
           return (
-            <li className="">
-              <Link to={navLink.path} className="no-underline text-[#333f27]">
+            <li className="py-[4vh] hover:bg-[#f5d3e4]">
+              <Link to={navLink.path} className="moblink">
                 {navLink.title}
               </Link>
             </li>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,45 +1,49 @@
-import React from 'react'
-import HeroSection from '../../components/HeroSection'
-import Footer from '../../components/Footer'
-import { Link } from 'react-router-dom';
+import React from "react";
+import HeroSection from "../../components/HeroSection";
+import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
-      <div>
-          <HeroSection />
+    <div>
+      <HeroSection />
 
-           <div id="Login" class="flex justify-center items-center flex-col h-[45vh] bg-[#ecdce4]">
-              <div>
-              <h1 class="tracking-[6px] font-NanumGothic text-3xl font-[600]">CUSTOMER LOGIN</h1>
-              </div>
-              <form> 
-                <input
-                  type="text"
-                  name="email"
-                  placeholder="Email"
-                  class="txt-box mt-[20px]"
-              /><br />
-              <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"  
-                  class="txt-box mt-[10px]"
-              />
-              </form>
-              
+      <div
+        id="Login"
+        class="flex justify-center items-center flex-col h-[45vh] bg-[#ecdce4]"
+      >
+        <div>
+          <h1 class="tracking-[6px] font-NanumGothic text-3xl font-[600]">
+            CUSTOMER LOGIN
+          </h1>
+        </div>
+        <form>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            class="txt-box mt-[20px]"
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            class="txt-box mt-[10px]"
+          />
+        </form>
 
-             <button class="btn-style">Login</button>
-             <p class="mt-[30px] no-underline tracking-[1.5px] font-NanumGothic">Not a member? <Link to='/signup'  class="no-underline font-NanumGothic">Signup</Link></p>
-
-            </div>
-
-          <Footer />
+        <button class="btn-style">Login</button>
+        <p class="mt-[30px] no-underline tracking-[1.5px] font-NanumGothic">
+          Not a member?{" "}
+          <Link to="/signup" class="no-underline font-NanumGothic">
+            Signup
+          </Link>
+        </p>
       </div>
-      
-      
 
-      
+      <Footer />
+    </div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
