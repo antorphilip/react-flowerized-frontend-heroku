adb58d16050c2b98427451517da4381da56938a2
aeschyllus <lapadamarkanthony@gmail.com>
2022-08-09 21:59:27 +0800

    Background image for contact us page

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -1,67 +1,65 @@
 import React from "react";
 import Footer from "../../components/Footer";
-import bgimage from "../../bgimage.png";
+import styles from "../../contact.module.css";
 
 const ContactUs = () => {
   return (
     <>
       <div className="h-[1px] mt-[80px]"></div>
       <div className="">
-        <div className="flex items-center justify-center">
+        <div
+          className={`flex items-center justify-center bg-blue-300 ${styles.bgimage}`}
+        >
           <div className="w-[90%] lg:w-2/3 flex my-28 rounded-xl shadow-xl overflow-hidden">
             <img
               className="lg:block hidden max-w-[500px]"
-              // src="./image/contact.jpg"
-              src={bgimage}
+              src="./image/contact.jpg"
               alt="logo"
-            />      
+            />
 
-          <div className="px-7 py-14 w-full text-center">
-            <h1 className="text-[#694E4E] tracking-[10px] text-3xl md:text-4xl uppercase font-ElMessiri font-bold ">
-              CONTACT US
-            </h1>
-            <p className=" text-2xl text-[#694E4E] tracking-[1px] font-Courgette">
-              Want to stay connected? Reach out to us!
-            </p>
-            {/* <p className=" text-2xl text-[#694E4E] tracking-[1px] font-Courgette">
-              Reach out to us!
-            </p> */}
-            <div className="mt-14 space-y-7">
-              <div className="flex space-x-5 items-center">
-              <a href="https://www.facebook.com/bloomsbymarra">
-                <i className="fa-brands fa-facebook text-6xl mb-3 text-[#694E4E] hover:text-[#C1A3A3] hover:scale-[1.1] transition-[2.5]"></i>
-              </a>
-              <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
-                Chat with us to our Official Facebook Page
+            <div className="px-7 py-14 w-full text-center bg-white">
+              <h1 className="text-[#694E4E] tracking-[10px] text-3xl md:text-4xl uppercase font-ElMessiri font-bold ">
+                CONTACT US
+              </h1>
+              <p className=" text-2xl text-[#694E4E] tracking-[1px] font-Courgette">
+                Want to stay connected? Reach out to us!
               </p>
-              </div>
-              <div className="flex space-x-5 items-center">
-              <a href="https://www.facebook.com/bloomsbymarra">
-                <i className="fa-brands fa-instagram text-6xl text-[#694E4E] hover:text-[#C1A3A3] hover:scale-[1.1] transition-[2.5]"></i>
-              </a>
-              <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
-                Slides thru DM to our Official Instagram Account
-              </p>
-              </div>
-              <div className="flex space-x-5 items-center">
-              <i className="fa-solid fa-phone text-6xl text-[#694E4E] hover:text-[#C1A3A3] hover:scale-[1.1] transition-[2.5]"></i>
-              <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
-                Give us a call/text: +639996040668
-              </p>
-              </div>
-              
-              <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
-                Or you can visit us at our physical store located at:
-              </p>
-              <div className="flex justify-center">
-                <img
-                  className="rounded border-[7px] border-[#C1A3A3] sm:w-[40%] object-contain w-[85%] mb-4 ml-3 hover:scale-[1.5] transition-[2.5]"
-                  src="./image/maps.png"
-                  alt="Map"
-                />
+              <div className="mt-14 space-y-7">
+                <div className="flex space-x-5 items-center">
+                  <a href="https://www.facebook.com/bloomsbymarra">
+                    <i className="fa-brands fa-facebook text-6xl mb-3 text-[#694E4E] hover:text-[#C1A3A3] hover:scale-[1.1] transition-[2.5]"></i>
+                  </a>
+                  <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
+                    Chat with us to our Official Facebook Page
+                  </p>
+                </div>
+                <div className="flex space-x-5 items-center">
+                  <a href="https://www.facebook.com/bloomsbymarra">
+                    <i className="fa-brands fa-instagram text-6xl text-[#694E4E] hover:text-[#C1A3A3] hover:scale-[1.1] transition-[2.5]"></i>
+                  </a>
+                  <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
+                    Slides thru DM to our Official Instagram Account
+                  </p>
+                </div>
+                <div className="flex space-x-5 items-center">
+                  <i className="fa-solid fa-phone text-6xl text-[#694E4E] hover:text-[#C1A3A3] hover:scale-[1.1] transition-[2.5]"></i>
+                  <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
+                    Give us a call/text: +639996040668
+                  </p>
+                </div>
+
+                <p className="font-Alata text-[#694E4E] tracking-[1px] text-xl">
+                  Or you can visit us at our physical store located at:
+                </p>
+                <div className="flex justify-center">
+                  <img
+                    className="rounded border-[7px] border-[#C1A3A3] sm:w-[40%] object-contain w-[85%] mb-4 ml-3 hover:scale-[1.5] transition-[2.5]"
+                    src="./image/maps.png"
+                    alt="Map"
+                  />
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
 
